d23539acfc143f465cbd2de8a9d9c58655b41c16
Asikur Rahaman <mdasikur5893@gmail.com>
2026-07-23 02:03:00 +0600

    get cars data in db

--- index.js
+++ index.js
@@ -22,8 +22,31 @@ async function run() {
             res.send(result);
         });
         //Todo get cars data in carsColl;
+        // app.get('/cars', async (req, res) => {
+        //     const catagory = req.query.category;
+        //     console.log(catagory);
+        //     const result = await carsColl.find().limit(3).sort({price: -1}).toArray()
+        //     res.send(result);
+        // });
         app.get('/cars', async (req, res) => {
-            const result = await carsColl.find().toArray()
+            const category = req.query.category;
+            console.log(category);
+
+            let query = {};
+
+            if (category) {
+                query.category = {
+                    $regex: category,
+                    $options: 'i'
+                };
+            }
+
+            const result = await carsColl
+                .find(query)
+                .sort({ price: -1 })
+                .limit(3)
+                .toArray();
+
             res.send(result);
         });
         //Todo root apis;
